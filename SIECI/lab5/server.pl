738c77bc3653499f0d75039fc9348b1a2a202377
CzadowyOlo <92742058+CzadowyOlo@users.noreply.github.com>
2022-06-12 21:35:06 +0200

    updated serber.pl
    lepsza obsluga requestoq headerow

--- SIECI/lab5/server.pl
+++ SIECI/lab5/server.pl
@@ -5,6 +5,8 @@ use HTTP::Status;
 use Data::Dumper;
 use IO::File;
 
+#$post =~ s/^\r*\n$/<br>/g;
+
 $Data::Dumper::Terse = 1;
 $Data::Dumper::Useqq = 1; 
 
@@ -17,9 +19,14 @@ print "Please contact me at: <URL:", $d->url, ">\n";
 
 # wpisuję sobie tekst do headera (na razie tylko na stringu), bo oprócz samego requesta wrzuce tam hiperłącze do indeks.html
 my $str = <<END;
-I AM header<br><a href="indeks.html"><strong>GO BACK</strong></a><br>
+I AM header<br><a href="indeks.html"><strong>GO BACK</strong></a><br><br>
 END
-
+my $strendline = <<END;
+<br>
+<br>
+next header: 
+<br>
+END
 # $SIG{INT} = \&tsktsk;
 # sub tsktsk {
 #     $SIG{INT} = \&tsktsk;           # See ``Writing A Signal Handler''
@@ -32,6 +39,10 @@ END
 
 
 my $dir = "./Web"; # w tym folderze mam podstrony mojej sieci
+my $filename = "./Web/header.html"; #odnosimy się do pliku indeks
+open(FH, '>', $filename) or die $!; #otwieramy
+print FH $str; #zrzucam wcześniej przygotowanego stringa do header.html
+close(FH); #zamykamy plik ze stroną html (header)
 
 while (my $c = $d->accept) { #sprawdzamy czy sie połaczyliśmy do witryny i zwracamy na c adres hosta albo 0
 
@@ -41,10 +52,14 @@ while (my $c = $d->accept) { #sprawdzamy czy sie połaczyliśmy do witryny i zwr
 
         if ($r->method eq 'GET') {
             my $header = $r->headers->as_string; #pobieramy rządanie ze zmiennej r i zapisujemy je pod stringiem
-            my $filename = "./Web/header.html"; #odnosimy się do pliku indeks
-            open(FH, '>', $filename) or die $!; #otwieramy
-            print FH $str; #zrzucam wcześniej przygotowanego stringa do header.html
-            print FH $header; #drukujemy do niego pobranego requesta
+            my $filename = "./Web/header.html"; #odnosimy się do pliku header
+            open(FH, '>>', $filename) or die $!; #otwieramy
+
+            foreach($r->header_field_names) {
+                print FH ($_. "\t----->\t" . $r->header($_) . "<br>" );
+            }
+
+            print FH $strendline;
             close(FH); #zamykamy plik ze stroną html (header)
 
             # r->uri A uniform resource identifier, 
